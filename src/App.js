d4869418768e5776829df28e025a57610eda313b
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2019-12-09 12:25:34 +0000

    Completed step 5 - add cartContext and given provider

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import ProductContext from "./contexts/ProductContext";
 import Navigation from "./components/Navigation";
 import Products from "./components/Products";
 import ShoppingCart from "./components/ShoppingCart";
+import cartContext from "./contexts/cartContext";
 
 function App() {
   const [products] = useState(data);
@@ -21,12 +22,14 @@ function App() {
   return (
     <div className="App">
       <ProductContext.Provider value={{ products, addItem }}>
-        <Navigation cart={cart} />
+        <cartContext.Provider value={cart}>
+          <Navigation cart={cart} />
 
-        {/* Routes */}
-        <Route exact path="/" component={Products} />
+          {/* Routes */}
+          <Route exact path="/" component={Products} />
 
-        <Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
+          <Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
+        </cartContext.Provider>
       </ProductContext.Provider>
     </div>
   );
